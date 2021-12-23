747839085824d4909cd228496f0661751b32a476
OsamaRaed <orashid303@gmail.com>
2021-12-23 17:16:41 +0200

    refactor to all internal commands

--- main.c
+++ main.c
@@ -5,11 +5,50 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 
 
 #define READ 0
 #define WRITE 1
+
+//will return bool 0 or 1
+bool isInternalCommand(char** command){
+	int isInternal = true;
+	printf("is internal command %s\n",command[0]);
+	if(strcmp("quit",command[0]) == 0) exit(0);
+	else if(strcmp("clc",command[0]) == 0){
+		isInternal = false;
+	    printf("\033[H\033[2J");
+
+	}
+	else if(strcmp("cd",command[0]) == 0){
+		isInternal = false;
+		chdir(command[1]);
+	}
+	else if(strcmp("help",command[0]) == 0){
+		isInternal = false;
+		system("bash -c help | more +10");
+	}
+	else if(strcmp("environ",command[0]) == 0){
+		isInternal = false;
+		printf("PATH : %s\n", getenv("PATH"));
+		
+	}
+	else if(strcmp("help",command[0]) == 0){
+		isInternal = false;
+		
+	}
+	else if(strcmp("help",command[0]) == 0){
+		isInternal = false;
+	    while(getchar() != '\n');
+	}
+	
+	
+	    while(getchar() != '\n');
+
+	printf("isInternal??%d",isInternal);
+}
+
 
 char* parseString(char * command, char **argv){
 
@@ -30,14 +69,19 @@ char* parseString(char * command, char **argv){
 
 
 void runShell(char* command){
-	char* argv[10];
+	printf("run shell\n");
+	char* argv[11];
 	parseString(command,argv);
-	if(fork() == 0){
-		//child
-		execvp(argv[0],argv);
-	} else {
-		//parent
-		wait(NULL);
+	bool condition = isInternalCommand(argv);
+	if(condition){
+		printf("is not internal\n");
+		if(fork() == 0){
+			//child
+			execvp(argv[0],argv);
+		} else {
+			//parent
+			wait(NULL);
+		}	
 	}
 }
 
@@ -78,109 +122,9 @@ int main (int argc, char *argv[]){
 		    
 		    // Consume the last \n so our next scan will wait ;)
 		    getchar ();
+		    runShell(command);
 		    
-		    // I've assumed that the max number of args is 10 + 1 (Last one should be NULL).
-		    char* args[11];
-			parseString(command,args);
-		    // Read about "strtok" in the manual pages (man strtok).
-//		    char* command_token = strtok (command, " ");
-		    
-//		    int argscounter = 0;
-		    //printf("first cm %s \n",command_token);
-//		    char* pch;
-//		    while (command_token != NULL && argscounter < 10) {
-	//	        if (argscounter != 0) {
-		            // You will need to split your string into small strings by replacing the space character by \0 character
-		//        	command_token = strtok(NULL," ");
-
-		  //      }
-		        //("%s\n",command_token);
-		        // I just pointed our array element to the start of the small string.
-		    //    args[argscounter] = command_token;
-		        
-		        // You should update the pointer to the next token, Read about "strtok" in the manual pages.
-		        //	command_token = ????;
-		      //  argscounter++;
-		    //}
-		    // Don't forget to make your last arg = NULL.
-			//args[argscounter] = NULL;
-			for(int i = 0; i <  10 && args[i] != NULL ;i++){
-				printf("%s\n",args[i]);
-			}
-			//}
-		    // Execute your command here, don't forget that you need to pass the full path, (e.g /usr/bin/mkdir)
-		    // Reprintfad about execv in the manula pages (man execv).
-			if(strcmp("quit",args[0]) == 0) exit(0);
-			if(strcmp("cd",args[0]) == 0)
-			{
-				if(chdir(args[1]) == -1){
-					printf("\n\nerrrrrrrrrrrrrrrrr\n\n\n\n\n");
-					continue;
-				}  
-				
-			}
-
-			else if(strcmp("help",args[0]) == 0){
-			
-				printf("\n\n\n\n\n\n\nwe are here");
-				system("bash -c help | more +10");
-				continue;
-			}
-			else if(strcmp("pause",args[0]) == 0){
-				char ch;
-				while(1){
-					ch = fgetc(stdin);
-					if(ch==0x0A)
-					{
-						break;
-					}
-					ch = getchar();
-
-				}
-
-			}
-			
-			if( strcmp("myshell",args[0]) == 0){
-				printf("test to open file\n");	
-				FILE * fp;
-				char * line = NULL;
-			    size_t len = 0;
-		        ssize_t read;
-				fp = fopen(args[1], "r");
-				if (fp == NULL){
-					printf("fail to open file\n");
-					continue;
-				}
-				    
-				while ((read = getline(&line, &len, fp)) != -1) {
-		    		printf("Retrieved line of length %zu:\n", read);
-		    		printf("%s", line);
-				}
-				//char* a  = teststrtok1(line);				
-				fclose(fp);
-				
-				if(fork() == 0){
-					//child
-					if (strcmp("environ",args[0]) == 0) execvp("env",args);
-					else execvp(args[0],args);
-					
-				} else {
-					//parent
-					wait(NULL);
-				}
-
-			} else {
-				if(fork() == 0){
-					//child
-				if (strcmp("environ",args[0]) == 0) execvp("env",args);
-				else execvp(args[0],args);
-				} else {
-					//parent
-					wait(NULL);
-				}
-			}
-
-		}	
+		}
 	}
     return 0;
 }
